b1c51af86263ce277a856e8ad3857b366ee4b30c
dasayo1215 <yamada.sayo1215@gmail.com>
2025-04-24 20:43:20 +0900

    完成

---
+++ src/resources/views/index.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/index.css') }}">
+@endsection
+
+@section('content')
+@if(session('message'))
+    <div class="todo-form__notice--success">{{ session('message') }}</div>
+@endif
+
+@if ($errors->any())
+    <ul class="todo-form__notice--error">
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+@endif
+
+<div class="todo-form__content">
+    <form class="form1" action="/todos" method="post">
+        @csrf
+        <div class="form1__input">
+            <div class="form1__input--text">
+                <input class="form1__input--text-input" type="text" name="content">
+            </div>
+            <div class="form1__input--button">
+                <button class="form1__input--button-submit" type="submit">作成</button>
+            </div>
+        </div>
+    </form>
+
+    <div class="todo__title">Todo</div>
+    @foreach ($todos as $todo)
+    <div class="form2">
+        <form class="update-form" action="/todos/update" method="post">
+            @method('PATCH')
+            @csrf
+            <input class="update-form__input-text" type="text" name="content" value="{{ $todo['content'] }}" >
+            <input type="hidden" name="id" value="{{ $todo['id'] }}">
+            <button class="update-form--button-submit" type="submit">更新</button>
+        </form>
+        <form class="delete-form" action="/todos/delete" method="post">
+            @method('delete')
+            @csrf
+            <input type="hidden" name="id" value="{{ $todo['id'] }}">
+            <button class="delete-form__button-submit" type="submit">削除</button>
+        </form>
+    </div>
+    @endforeach
+</div>
+@endsection()
